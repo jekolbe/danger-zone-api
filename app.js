dd5350ea163f9be950d26af1ab97193e193e16e6
Jenö <jenoe.kolbe@prodyna.com>
2024-02-20 23:04:15 +0100

    Added swagger ui

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var swaggerUi = require('swagger-ui-express');
 
 var indexRouter = require('./routes/index');
 var tripsRouter = require('./routes/trips');
 const connectDB = require('./config/db');
+const swaggerDocument = require('./swagger.json');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/trips', tripsRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
