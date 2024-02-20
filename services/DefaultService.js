5c045f9d6a57590a4524a2a19d7b2fd49a245ba8
Jenö <jenoe.kolbe@prodyna.com>
2024-02-20 22:19:38 +0100

    initialized openapi

---
+++ services/DefaultService.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+const Service = require('./Service');
+
+/**
+* List all trips
+*
+* returns List
+* */
+const tripsGET = () => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Get a trip by ID
+*
+* id Integer 
+* returns Trip
+* */
+const tripsIdGET = ({ id }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        id,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+
+module.exports = {
+  tripsGET,
+  tripsIdGET,
+};
