a46a44ea1303b3e295d9cb95c299712a9084e925
Jenö <jenoe.kolbe@prodyna.com>
2024-02-21 08:49:26 +0100

    Added nodemon for local development

--- models/trips.js
+++ models/trips.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const TripSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  dangerousness: String,
+    id: Number,
+    name: String,
+    description: String,
+    price: Number,
+    dangerousness: String,
 });
 
 module.exports = mongoose.model('Trip', TripSchema);
